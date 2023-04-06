8840499898744882aaf0ff2bd649742cf4d4d0bc
cllojan <cesar.lojan8@gmail.com>
2023-04-06 10:32:00 -0500

    add: tab header

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,19 @@
+const tabs = document.querySelectorAll("[data-target]"),
+    tabContents = document.querySelectorAll("[data-content]");    
 let toggleTheme = document.getElementById("toggleTheme");
-let icon = document.getElementById("icon");
+
+
+tabs.forEach(tab => {
+  tab.addEventListener("click",() => {    
+    console.log(tab.dataset.target)
+    const target = document.querySelector(tab.dataset.target)
+    const idTarget = document.getElementById(target);
+    tabContents.forEach(tc =>{
+      tc.classList.remove('options__active')
+    })
+    target.classList.add("options__active")
+  })
+})
 toggleTheme.addEventListener("click", (e) => {
     e.preventDefault();
     let element = document.body;    
