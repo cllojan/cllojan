7fa511952b57341ffc43f0e95349d00bb49ab40a
cllojan <cesar.lojan8@gmail.com>
2023-04-07 21:40:02 -0500

    cambio de idioma

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,49 @@
-const tabs = document.querySelectorAll("[data-target]"),
-    tabContents = document.querySelectorAll("[data-content]");    
+const tabs = document.querySelectorAll(".head[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
 let toggleTheme = document.getElementById("toggleTheme");
 
-
+const idiom = document.querySelector(".idioma[data-target]");
+const contentId = document.querySelectorAll("[data-content='idiom']");
 tabs.forEach(tab => {
-  tab.addEventListener("click",() => {    
-    console.log(tab.dataset.target)
+  tab.addEventListener("click", () => {
     const target = document.querySelector(tab.dataset.target)
-    const idTarget = document.getElementById(target);
-    tabContents.forEach(tc =>{
+    tabContents.forEach(tc => {
       tc.classList.remove('options__active')
     })
     target.classList.add("options__active")
   })
 })
 toggleTheme.addEventListener("click", (e) => {
-    e.preventDefault();
-    let element = document.body;    
-    element.classList.toggle("dark");    
-    if(icon.className=="fa-solid fa-moon"){
-        icon.className = "fa-solid fa-sun";
-      }else{
-        icon.className = "fa-solid fa-moon";
-      }        
+  e.preventDefault();
+  let element = document.body;
+  element.classList.toggle("dark");
+  if (icon.className == "fa-solid fa-moon") {
+    icon.className = "fa-solid fa-sun";
+  } else {
+    icon.className = "fa-solid fa-moon";
+  }
 })
+
+
+idiom.addEventListener("click", (tab) => {
+  
+  const target = document.querySelectorAll(tab.target.dataset.target)
+  contentId.forEach(tc => {
+    tc.classList.remove('desc__active')
+    console.log(tc.id)
+    
+  })      
+  tab.target.dataset.target = tab.target.dataset.target == "#en"  ? "#es"  :"#en"
+  target.forEach(tc =>{
+    tc.classList.add("desc__active")
+  })
+  
+})
+/*
+function idioma(event){
+  event.preventDefault();
+  console.log(event.target.dataset.target)
+  
+
+}
+*/
